bf5c4bee7a87399f28a59a7e8d224384760c21b8
Motinha <motinha11@outlook.pt>
2023-02-06 22:49:36 +0000

    HA-61: Wip;

---
+++ src/Presentation.WebAPI/Queries/Betano/GetBetanoFootballDataQuery/GetBetanoFootballDataQuery.cs
@@ -0,0 +1,21 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GetBetanoFootballDataQuery.cs" company="HumbleBets">
+//     Copyright (c) HumbleBets. All rights reserved.
+// </copyright>
+// <summary>
+// GetBetanoFootballDataQuery
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace BookmakerIntegration.Presentation.WebAPI.Queries.Betano.GetBetanoFootballDataQuery
+{
+    using MediatR;
+
+    /// <summary>
+    /// <see cref="GetBetanoFootballDataQuery"/>
+    /// </summary>
+    /// <seealso cref="INotification"/>
+    public class GetBetanoFootballDataQuery : INotification
+    {
+    }
+}
---
+++ src/Presentation.WebAPI/Queries/Betano/GetBetanoFootballDataQuery/GetBetanoFootballDataQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GetBetanoFootballDataQueryHandler.cs" company="HumbleBets">
+//     Copyright (c) HumbleBets. All rights reserved.
+// </copyright>
+// <summary>
+// GetBetanoFootballDataQueryHandler
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace BookmakerIntegration.Presentation.WebAPI.Queries.Betano.GetBetanoFootballDataQuery
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using MediatR;
+
+    public class GetBetanoFootballDataQueryHandler : INotificationHandler<GetBetanoFootballDataQuery>
+    {
+        public Task Handle(GetBetanoFootballDataQuery notification, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
