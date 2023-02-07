6bd6db6c774dba24790f35d1f9e06d0e97448878
Motinha <motinha11@outlook.pt>
2023-02-07 23:36:02 +0000

    HA-61: Add DataCollector Service;

---
+++ src/Presentation.WebAPI/Services/DataCollector/IDataCollector.cs
@@ -0,0 +1,25 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IDataCollector.cs" company="HumbleBets">
+//     Copyright (c) HumbleBets. All rights reserved.
+// </copyright>
+// <summary>
+// IDataCollector
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace BookmakerIntegration.Presentation.WebAPI.Services.DataCollector
+{
+    /// <summary>
+    /// IDataCollector
+    /// </summary>
+    public interface IDataCollector
+    {
+        /// <summary>
+        /// Collects the betano data asynchronous.
+        /// </summary>
+        /// <param name="url">The URL.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
+        Task<string> CollectBetanoDataAsync(string url, CancellationToken cancellationToken);
+    }
+}
