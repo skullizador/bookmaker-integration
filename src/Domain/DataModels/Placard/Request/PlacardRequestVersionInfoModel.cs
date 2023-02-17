6aa400ece1222527fe445765b738c41bce55ba3c
Motinha <motinha11@outlook.pt>
2023-02-17 23:26:35 +0000

    HA-62: Add Placard Request Model;

---
+++ src/Domain/DataModels/Placard/Request/PlacardRequestVersionInfoModel.cs
@@ -0,0 +1,31 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PlacardRequestVersionInfoModel.cs" company="HumbleBets">
+//     Copyright (c) HumbleBets. All rights reserved.
+// </copyright>
+// <summary>
+// PlacardRequestVersionInfoModel
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace BookmakerIntegration.Domain.DataModels.Placard.Request
+{
+    using BookmakerIntegration.Domain.ConstantCollections.Placard;
+
+    /// <summary>
+    /// <see cref="PlacardRequestVersionInfoModel"/>
+    /// </summary>
+    public class PlacardRequestVersionInfoModel
+    {
+        /// <summary>
+        /// Gets the API version.
+        /// </summary>
+        /// <value>The API version.</value>
+        public string apiVersion => PlacardConstantCollection.ApiVersion.Value;
+
+        /// <summary>
+        /// Gets the module version.
+        /// </summary>
+        /// <value>The module version.</value>
+        public string moduleVersion => PlacardConstantCollection.ModuleVersion.Value;
+    }
+}
---
+++ src/Domain/DataModels/Placard/Request/PlacardVariablesModel.cs
@@ -0,0 +1,48 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PlacardVariablesModel.cs" company="HumbleBets">
+//     Copyright (c) HumbleBets. All rights reserved.
+// </copyright>
+// <summary>
+// PlacardVariablesModel
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace BookmakerIntegration.Domain.DataModels.Placard.Request
+{
+    /// <summary>
+    /// <see cref="PlacardVariablesModel"/>
+    /// </summary>
+    public class PlacardVariablesModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PlacardVariablesModel"/> class.
+        /// </summary>
+        /// <param name="selectedCompetitionId">The selected competition identifier.</param>
+        /// <param name="selectedDate">The selected date.</param>
+        /// <param name="selectedModalityId">The selected modality identifier.</param>
+        public PlacardVariablesModel(string selectedCompetitionId, string selectedDate, string selectedModalityId)
+        {
+            this.SelectedCompetitionId = selectedCompetitionId;
+            this.SelectedDate = selectedDate;
+            this.SelectedModalityId = selectedModalityId;
+        }
+
+        /// <summary>
+        /// Gets the selected competition identifier.
+        /// </summary>
+        /// <value>The selected competition identifier.</value>
+        public string SelectedCompetitionId { get; init; }
+
+        /// <summary>
+        /// Gets the selected date.
+        /// </summary>
+        /// <value>The selected date.</value>
+        public string SelectedDate { get; init; }
+
+        /// <summary>
+        /// Gets the selected modality identifier.
+        /// </summary>
+        /// <value>The selected modality identifier.</value>
+        public string SelectedModalityId { get; init; }
+    }
+}
