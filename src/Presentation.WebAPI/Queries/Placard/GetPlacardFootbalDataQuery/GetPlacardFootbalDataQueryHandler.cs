5e1ca820ebbf9f7fd568f8c7b5c3d769b04708f7
Motinha <motinha11@outlook.pt>
2023-02-17 23:28:46 +0000

    HA-62: Implement Get Placard Footbal Data Query:

---
+++ src/Presentation.WebAPI/Queries/Placard/GetPlacardFootbalDataQuery/GetPlacardFootbalDataQueryHandler.cs
@@ -0,0 +1,64 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GetPlacardFootbalDataQueryHandler.cs" company="HumbleBets">
+//     Copyright (c) HumbleBets. All rights reserved.
+// </copyright>
+// <summary>
+// GetPlacardFootbalDataQueryHandler
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace BookmakerIntegration.Presentation.WebAPI.Queries.Placard.GetPlacardFootbalDataQuery
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using BookmakerIntegration.Domain.DataModels.Placard.Request;
+    using BookmakerIntegration.Domain.DataModels.Placard.Response;
+    using BookmakerIntegration.Presentation.WebAPI.Services.DataCollector;
+    using MediatR;
+
+    /// <summary>
+    /// <see cref="GetPlacardFootbalDataQueryHandler"/>
+    /// </summary>
+    /// <seealso cref="IRequestHandler{GetPlacardFootbalDataQuery, PlacardResponseModel}"/>
+    public class GetPlacardFootbalDataQueryHandler : IRequestHandler<GetPlacardFootbalDataQuery, PlacardResponseModel>
+    {
+        /// <summary>
+        /// The data collector
+        /// </summary>
+        private readonly IDataCollector dataCollector;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetPlacardFootbalDataQueryHandler"/> class.
+        /// </summary>
+        /// <param name="dataCollector">The data collector.</param>
+        public GetPlacardFootbalDataQueryHandler(IDataCollector dataCollector)
+        {
+            this.dataCollector = dataCollector;
+        }
+
+        /// <summary>
+        /// Handles a request
+        /// </summary>
+        /// <param name="request">The request</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>Response from the request</returns>
+        public async Task<PlacardResponseModel> Handle(GetPlacardFootbalDataQuery request, CancellationToken cancellationToken)
+        {
+            PlacardVariablesModel variables = new(
+                request.PlacardCompetitionId.ToString(),
+                request.Date.ToString("yyyy-MM-dd"),
+                request.Sport);
+
+            PlacardRequestModel body = new(variables);
+
+            string url = "https://placard.jogossantacasa.pt/PlacardWeb/screenservices/Plcard_WebEvents_CW/EventsFlow/EventList_EventListBlock/DataActionGetEventsData";
+
+            PlacardResponseModel data = await this.dataCollector.CollectPlacardDataAsync(
+                url,
+                body,
+                cancellationToken);
+
+            return data;
+        }
+    }
+}
