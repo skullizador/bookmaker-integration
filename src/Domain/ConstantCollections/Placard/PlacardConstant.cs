76ad3edc45d77bdabf0fe945f0ba5b6e8138fa2d
Motinha <motinha11@outlook.pt>
2023-02-17 23:25:31 +0000

    HA-62: Implement Placard Constant Collection;

---
+++ src/Domain/ConstantCollections/Placard/PlacardConstant.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PlacardConstant.cs" company="HumbleBets">
+//     Copyright (c) HumbleBets. All rights reserved.
+// </copyright>
+// <summary>
+// PlacardConstant
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace BookmakerIntegration.Domain.ConstantCollections.Placard
+{
+    using BookmakerIntegration.Domain.Utils;
+
+    /// <summary>
+    /// <see cref="PlacardConstant"/>
+    /// </summary>
+    /// <seealso cref="ConstantObject"/>
+    public class PlacardConstant : ConstantObject
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PlacardConstant"/> class.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="value">The value.</param>
+        public PlacardConstant(string name, string value)
+            : base(value)
+        {
+            this.Name = name;
+        }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>The name.</value>
+        public string Name { get; set; }
+    }
+}
---
+++ src/Domain/ConstantCollections/Placard/PlacardConstantCollection.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PlacardConstantCollection.cs" company="HumbleBets">
+//     Copyright (c) HumbleBets. All rights reserved.
+// </copyright>
+// <summary>
+// PlacardConstantCollection
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace BookmakerIntegration.Domain.ConstantCollections.Placard
+{
+    /// <summary>
+    /// <see cref="PlacardConstantCollection"/>
+    /// </summary>
+    public static class PlacardConstantCollection
+    {
+        /// <summary>
+        /// Gets the API version.
+        /// </summary>
+        /// <value>The API version.</value>
+        public static PlacardConstant ApiVersion => new("apiVersion", "+CXbYZ1y22VBdCuen25vAg");
+
+        /// <summary>
+        /// Gets the module version.
+        /// </summary>
+        /// <value>The module version.</value>
+        public static PlacardConstant ModuleVersion => new("moduleVersion", "xZ7ndd+e16MkGvtoGztwxA");
+
+        /// <summary>
+        /// Gets the name of the request view.
+        /// </summary>
+        /// <value>The name of the request view.</value>
+        public static PlacardConstant RequestViewName => new("viewName", "Events.Events");
+
+        /// <summary>
+        /// Gets the request XCSRF token header.
+        /// </summary>
+        /// <value>The request XCSRF token header.</value>
+        public static PlacardConstant RequestXCSRFTokenHeader => new("x-csrftoken", "T6C+9iB49TLra4jEsMeSckDMNhQ=");
+    }
+}
