fbadbcd349b85803d91f82fcd70bc9462105e121
Motinha <motinha11@outlook.pt>
2023-02-24 21:20:41 +0000

    HA-63: Add bwin functionatily;

---
+++ src/Presentation.WebAPI/Controller/BwinController.cs
@@ -0,0 +1,78 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BwinController.cs" company="HumbleBets">
+//     Copyright (c) HumbleBets. All rights reserved.
+// </copyright>
+// <summary>
+// BwinController
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace BookmakerIntegration.Presentation.WebAPI.Controller
+{
+    using System.Net;
+    using AutoMapper;
+    using BookmakerIntegration.Domain.DataModels.Bwin;
+    using BookmakerIntegration.Presentation.WebAPI.Dtos.Input.Bookmaker;
+    using BookmakerIntegration.Presentation.WebAPI.Dtos.Output.Bookmaker;
+    using BookmakerIntegration.Presentation.WebAPI.Mappers.Bwin;
+    using BookmakerIntegration.Presentation.WebAPI.Queries.Bwin.GetBwinFootballDataQuery;
+    using BookmakerIntegration.Presentation.WebAPI.Utils;
+    using MediatR;
+    using Microsoft.AspNetCore.Mvc;
+
+    /// <summary>
+    /// <see cref="BwinController"/>
+    /// </summary>
+    /// <seealso cref="Controller"/>
+    [Route("api/v1/Bwin")]
+    [ApiController]
+    public class BwinController : Controller
+    {
+        /// <summary>
+        /// The mapper
+        /// </summary>
+        private readonly IMapper mapper;
+
+        /// <summary>
+        /// The mediator
+        /// </summary>
+        private readonly IMediator mediator;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BwinController"/> class.
+        /// </summary>
+        /// <param name="mapper">The mapper.</param>
+        /// <param name="mediator">The mediator.</param>
+        public BwinController(
+            IMapper mapper,
+            IMediator mediator)
+        {
+            this.mapper = mapper;
+            this.mediator = mediator;
+        }
+
+        /// <summary>
+        /// Gets the football competition data asynchronous.
+        /// </summary>
+        /// <param name="filter">The filter.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("Football/{CompetitionId}")]
+        [ProducesResponseType(typeof(CompetitionDto), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ErrorMessage), (int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(ErrorMessage), (int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> GetFootballCompetitionDataAsync(
+            [FromRoute] GetBookmakerDataByCompetitionIdDto filter,
+            CancellationToken cancellationToken)
+        {
+            BwinWidgetDataModel data = await this.mediator.Send(new GetBwinFootballDataQuery
+            {
+                CompetitionId = filter.CompetitionId,
+            }, cancellationToken);
+
+            //TODO: FIX BOOKMAKERID AND LEAGUE;
+            return this.Ok(data.MapToCompetitionDto(Guid.Empty, ""));
+        }
+    }
+}
---
+++ src/Presentation.WebAPI/Queries/Bwin/GetBwinFootballDataQuery/GetBwinFootballDataQueryHandler.cs
@@ -0,0 +1,65 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GetBwinFootballDataQueryHandler.cs" company="HumbleBets">
+//     Copyright (c) HumbleBets. All rights reserved.
+// </copyright>
+// <summary>
+// GetBwinFootballDataQueryHandler
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace BookmakerIntegration.Presentation.WebAPI.Queries.Bwin.GetBwinFootballDataQuery
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using BookmakerIntegration.Domain.AggregateModels.Bookmaker.Repository;
+    using BookmakerIntegration.Domain.ConstantCollections.Bwin;
+    using BookmakerIntegration.Domain.DataModels.Bwin;
+    using BookmakerIntegration.Presentation.WebAPI.Services.DataCollector;
+    using MediatR;
+
+    /// <summary>
+    /// <see cref="GetBwinFootballDataQueryHandler"/>
+    /// </summary>
+    /// <seealso cref="IRequestHandler{GetBwinFootballDataQuery, BwinWidgetDataModel}"/>
+    public class GetBwinFootballDataQueryHandler : IRequestHandler<GetBwinFootballDataQuery, BwinWidgetDataModel>
+    {
+        /// <summary>
+        /// The competition repository
+        /// </summary>
+        private readonly ICompetitionRepository competitionRepository;
+
+        /// <summary>
+        /// The data collector
+        /// </summary>
+        private readonly IDataCollector dataCollector;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetBwinFootballDataQueryHandler"/> class.
+        /// </summary>
+        /// <param name="competitionRepository">The competition repository.</param>
+        /// <param name="dataCollector">The data collector.</param>
+        public GetBwinFootballDataQueryHandler(
+            ICompetitionRepository competitionRepository,
+            IDataCollector dataCollector)
+        {
+            this.competitionRepository = competitionRepository;
+            this.dataCollector = dataCollector;
+        }
+
+        /// <summary>
+        /// Handles a request
+        /// </summary>
+        /// <param name="request">The request</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>Response from the request</returns>
+        public async Task<BwinWidgetDataModel> Handle(GetBwinFootballDataQuery request, CancellationToken cancellationToken)
+        {
+            BwinJsonDataModel data = await this.dataCollector.CollectBwinDataAsync(
+                "https://sports.bwin.pt/pt/sports/api/widget?sportId=4&regionId=37&competitionId=102851&compoundCompetitionId=2:102851&forceFresh=1",
+                cancellationToken);
+
+            return data.widgets
+                .First(x => x.templateName == BwinConstantCollection.CurrentWantedWidgetTemplateName.Value);
+        }
+    }
+}
